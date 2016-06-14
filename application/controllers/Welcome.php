6e0da7026d39442a4d9e277a59f9b5c46c40bd14
Son Vi Hoang <hoang_son@lampart-vn.com>
2016-06-14 11:58:57 +0700

    commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,67 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->index2();
+		return;
+		$this->load->library('doctrine');
+
+		$group = new Entity\UserGroup;
+		$group->setName('Users');
+
+		$user = new Entity\User;
+		$user->setUsername('wildlyinaccurate');
+		$user->setPassword('Passw0rd');
+		$user->setEmail('wildlyinaccurate@gmail.com');
+		$user->setGroup($group);
+
+		// When you have set up your database, you can persist these entities:
+		// $em = $this->doctrine->em;
+		// $em->persist($group);
+		// $em->persist($user);
+		// $em->flush();
+
+		$this->load->view('welcome_message', array(
+			'user' => $user,
+			'group' => $group,
+		));
+	}
+
+	public function index2()
+	{
+		$this->load->library('doctrine');
+		$group = new Entity\UserGroup;
+		d($group->getUsers()->toArray());
+		$em = $this->doctrine->em;
+		$groups = $em->getRepository("Entity\UserGroup")->findAll();
+		foreach ($groups as $key => $value) {
+			$user = $value->getUsers();
+			foreach ($user as $key2 => $value2) {
+				echo $value2->getId();
+			}
+		}
+		$user = $em->getRepository("Entity\User")->findAll();
+		foreach ($user as $key => $value) {
+			d($value->getGroup()->getName());
+		}
+		
+	}
+}
